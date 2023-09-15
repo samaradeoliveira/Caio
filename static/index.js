45f22f7b096f019c9ccc81363625dbfd191cbaeb
Oliver <95024811+samaradeoliveira@users.noreply.github.com>
2023-09-15 15:57:00 -0300

    Update index.js

--- static/index.js
+++ static/index.js
@@ -20,12 +20,12 @@ $(function () {
             dataType: "json",
             contentType: 'application/json',
             success: function (result) {
-                $("#prediction").html(result.data.predicted_emotion)
-                $("#emo_img_url").attr('src', result.data.predicted_emotion_img_url);
-                $('#prediction').css("display", "");
-                $('#emo_img_url').css("display", "");
-                predicted_emotion = result.data.predicted_emotion
-                $('#save_button').prop('disabled', false);
+
+
+
+
+
+                
             },
             error: function (result) {
                 alert(result.responseJSON.message)
@@ -33,20 +33,19 @@ $(function () {
         });
     });
     $("#save_button").click(function () {
-        save_data = {
-            "date": new_date,
-            "text": $("#text").val(),
-            "emotion": predicted_emotion
+
+
+
+        
         }
         $.ajax({
-            type: 'POST',
-            url: "/save-entry",
-            data: JSON.stringify(save_data),
-            dataType: "json",
-            contentType: 'application/json',
-            success: function () {
-                alert("Sua entrada foi salva com sucesso!")
-                window.location.reload()
+
+
+
+
+
+
+        
             },
             error: function (result) {
                 alert(result.responseJSON.message)
